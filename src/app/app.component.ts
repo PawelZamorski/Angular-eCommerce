960672ced802d2b2feaa60528007300e35843963
Pawel <zamorskipawel@wp.pl>
2021-01-07 15:21:02 +0000

    Add redirection after the user logged in to the page. Redirects to the home page or to the page that was not accessible for not logged-in user that user was trying to access using url.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'; 
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerce';
+  
+  constructor(private auth: AuthService, private router: Router ) {
+    // Redirect to the page from which the user was redirected 
+    // when trying to access the page that are only accessible for logged in user.
+    auth.user$.subscribe(user => {
+      // Navigate to the page only if the user is logged in
+      if (user) {
+        let returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl);
+      }
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,14 @@ import firebase from 'firebase';
 export class AuthService {
 
   public user$: Observable<firebase.User>;
-  constructor(private auth: AngularFireAuth) { 
+  constructor(private auth: AngularFireAuth, private route: ActivatedRoute) { 
     this.user$ = auth.authState;
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
 
